f28c9e3154b5ddaa1e58e3dcbdacc54de4377657
Olatoyan <golatoyan@gmail.com>
2023-10-03 22:51:51 +0100

    fixed a little error

--- src/components/BackCard.js
+++ src/components/BackCard.js
@@ -1,7 +1,7 @@
 export default function BackCard({ formData }) {
   return (
     <div className="back__card">
-      <p className="back__number">{formData.cvc}</p>
+      <p className="back__number">{formData.cvc ? formData.cvc : "000"}</p>
     </div>
   );
 }
